76fe78da2c3431bdb98248f51437ed60e498b180
Antoine BRICHARD <abrichar@e1r10p5.42.fr>
2018-06-14 19:25:37 +0200

    maj

--- libft/src/ft_strsub.c
+++ libft/src/ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: abrichar <abrichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/14 12:44:32 by abrichar          #+#    #+#             */
-/*   Updated: 2016/11/23 04:47:07 by abrichar         ###   ########.fr       */
+/*   Updated: 2018/06/14 19:24:31 by abrichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,10 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len)
 	char		*strnew;
 	size_t		index;
 
-	if (s != 0)
+	if (s)
 	{
-		if (!(char *)malloc(sizeof(char) * (len + 1)))
+		if (!(strnew = (char *)malloc(sizeof(char) * (len + 1))))
 			return (0);
-		strnew = (char *)malloc(sizeof(char) * (len + 1));
 		index = 0;
 		while (index < len)
 		{
